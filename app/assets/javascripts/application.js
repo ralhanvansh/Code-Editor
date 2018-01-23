a7169b80f6088b1e207c7068ed48826909f5b9d2
ralhanvansh5 <ralhanvansh5@gmail.com>
2018-01-23 21:00:52 +0530

    Html/css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,38 +12,64 @@
 //
 //= require jquery
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
 
 
 $(document).ready(function(){
 
-  var html = $("#HtmlText").load("html.txt");
-  var css = $("#CSSText").load("css.txt ");
-  var js = $("#JSText").load("js.txt");
+  // $("#HtmlText").load("html.txt");
+  // $("#CSSText").load("css.txt");
+  // $("#JSText").load("js.txt");
 
-  var result = $("#ResultArea").contents().find("body");
-  result.html('<span id="htmlpart"></span><style id="csspart"></style><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" id="jspart"></script>');
+      var defaultHTMLInput = "<div class='loader'></div>\n<div class='loader second'></div>";
+			var defaultCSSInput =  ".loader{"+
+    								"\nbackground:grey;"+
+			    					"\nborder: 4px dashed white;"+
+    								"\nborder-radius: 50%;"+
+    								"\nwidth: 100px;"+
+								"\nheight: 100px;"+
+								"\nanimation: spin 15s linear infinite;"+
+								"\nmargin:0 auto;"+
+								"\nmargin-top:55px;"+
+								"\n}"+
+								"\n@keyframes spin {"+
+								    "\n0% { transform: rotate(0deg); }"+
+								    "\n100% { transform: rotate(360deg);"+
+								    "}"+
+								 "\n }"+
+								"\n\n.second {"+
+			    					"\nbackground-color:tomato;"+
+			    					"\nposition:relative;"+
+			    					"\nheight:50px;"+
+			    					"\nwidth:50px;"+
+			    					"\nanimation-duration:8s;"+
+								"\ntop:-67px;"+
+			    					"\nright:42px;"+
+			    					"\nanimation-direction:reverse;"+
+			    					"}";
 
-  result.find("#htmlpart").html(html);
-  result.find("#csspart").html(css);
-  result.find("#jspart").html(js);
+
+  $("#HtmlText").html(defaultHTMLInput);
+  $("#CSSText").html(defaultCSSInput);
+
+  $("#ResultArea").append( "<div id = 'htmlpart'>" +  defaultHTMLInput + "</div>",'<div id = "csspart"><style>' + defaultCSSInput + "</style></div>", "<div id='jspart'><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'>" + "</script></div>");
 
   $("#HtmlText").on('keyup', function(){
-    result.find("#htmlpart").html($(this).val());
+    $("#ResultArea").find("#htmlpart").html($(this).val());
   });
 
   $("#CSSText").on('keyup', function(){
-    result.find("#csspart").html($(this).val());
+    $("#ResultArea").find("#csspart").html("<style>" + $(this).val() + "</style>");
   });
 
-  $("#JSText").on('keyup', function(){
-    result.find("#jspart").html($(this).val());
+  $("#JSText").on('change', function(){
+    $("#ResultArea").find("#jspart").remove();
+    $("#ResultArea").append('<div id="jspart"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">'+ $(this).val() + '</script></div>');
   });
 
   $("#btn-result").on('click',function(){
-	  p.find("#jspart").remove();
-	  p.append('<script  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" id="jspart">'+$("#jscontainer").val()+'</script>');
+    $("#ResultArea").find("#jspart").remove();
+    $("#ResultArea").append('<div id="jspart"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">'+ $("#JSText").val() + '</script></div>');
 	 });
 });
