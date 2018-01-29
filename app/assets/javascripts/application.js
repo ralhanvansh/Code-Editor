cf2b8b9f3b148bb34efa52ad5107bd6c5528a05e
ralhanvansh5 <ralhanvansh5@gmail.com>
2018-01-29 12:20:06 +0530

    JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,38 +19,39 @@ $(document).ready(function(){
   // $("#HtmlText").load("html.txt");
   // $("#CSSText").load("css.txt");
   // $("#JSText").load("js.txt");
-      var defaultHTMLInput = "<div class='loader'></div>\n<div class='loader second'></div>";
-			var defaultCSSInput =  ".loader{"+
-    								"\nbackground:grey;"+
-			    					"\nborder: 4px dashed white;"+
-    								"\nborder-radius: 50%;"+
-    								"\nwidth: 100px;"+
-								"\nheight: 100px;"+
-								"\nanimation: spin 15s linear infinite;"+
-								"\nmargin:0 auto;"+
-								"\nmargin-top:55px;"+
-								"\n}"+
-								"\n@keyframes spin {"+
-								    "\n0% { transform: rotate(0deg); }"+
-								    "\n100% { transform: rotate(360deg);"+
-								    "}"+
-								 "\n }"+
-								"\n\n.second {"+
-			    					"\nbackground-color:tomato;"+
-			    					"\nposition:relative;"+
-			    					"\nheight:50px;"+
-			    					"\nwidth:50px;"+
-			    					"\nanimation-duration:8s;"+
-								"\ntop:-67px;"+
-			    					"\nright:42px;"+
-			    					"\nanimation-direction:reverse;"+
-			    					"}";
 
+  var defaultHTMLInput = "<div class='loader'></div>\n<div class='loader second'></div>";
+	var defaultCSSInput =  ".loader{"+
+								"\nbackground:grey;"+
+	    					"\nborder: 4px dashed white;"+
+								"\nborder-radius: 50%;"+
+								"\nwidth: 100px;"+
+						"\nheight: 100px;"+
+						"\nanimation: spin 15s linear infinite;"+
+						"\nmargin:0 auto;"+
+						"\nmargin-top:55px;"+
+						"\n}"+
+						"\n@keyframes spin {"+
+						    "\n0% { transform: rotate(0deg); }"+
+						    "\n100% { transform: rotate(360deg);"+
+						    "}"+
+						 "\n }"+
+						"\n\n.second {"+
+	    					"\nbackground-color:tomato;"+
+	    					"\nposition:relative;"+
+	    					"\nheight:50px;"+
+	    					"\nwidth:50px;"+
+	    					"\nanimation-duration:8s;"+
+						"\ntop:-67px;"+
+	    					"\nright:42px;"+
+	    					"\nanimation-direction:reverse;"+
+	    					"}";
   $("#HtmlText").html(defaultHTMLInput);
   $("#CSSText").html(defaultCSSInput);
 
-  $("#ResultArea").append( "<div id = 'htmlpart'>" +  defaultHTMLInput + "</div>",'<div id = "csspart"><style>' + defaultCSSInput + "</style></div>", "<div id='jspart'><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script></div>");
+  $("#ResultArea").append( "<div id='jspart'><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'>" + "</script></div>", "<div id = 'htmlpart'>" +  defaultHTMLInput + "</div>",'<div id = "csspart"><style>' + defaultCSSInput + "</style></div>");
   // $('#htmlpart').append('<div id="jspart"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">' + "</script></div>");
+
   $("#HtmlText").on('keyup', function(){
     $("#ResultArea").find("#htmlpart").remove();
     $("#ResultArea").append("<div id = 'htmlpart'>" +  $(this).val() + "</div>");
@@ -65,6 +66,7 @@ $(document).ready(function(){
     $("#ResultArea").empty();
     $('#ResultArea').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" id="jspart" >' + $(this).val() + "</script>" + "<div id = 'htmlpart'>" +  $("#HtmlText").val() + "</div>" + '<div id = "csspart"><style>' + $("#CSSText").val() + "</style></div>");
   });
+
   // $("#btn-result").on('click',function(){
   //   $("#ResultArea").empty();
   //   $("#ResultArea").append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" id="jspart" >' + $("#jspart").val() + "</script>" + "<div id = 'htmlpart'>" +  $("#HtmlText").val() + "</div>" + '<div id = "csspart"><style>' + $("#CSSText").val() + "</style></div>");
